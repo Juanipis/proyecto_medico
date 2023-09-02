9ec3f53e14815661ff8acf7015a22b7937f084c6
Juan Pablo Díaz Correa <juanipis@gmail.com>
2023-09-01 22:21:34 -0500

    wip patient data
    sexo, peso y edad

--- flutter/lib/presentation/screens/patient_data/patient_data.dart
+++ flutter/lib/presentation/screens/patient_data/patient_data.dart
@@ -1,26 +1,101 @@
 import 'package:flutter/material.dart';
 
-import '../data_input_mode/data_input_mode.dart';
+class PatientData extends StatefulWidget {
+  const PatientData({super.key});
 
-class PatientData extends StatelessWidget {
+  @override
+  State<PatientData> createState() => _PatientDataState();
+}
+
+class _PatientDataState extends State<PatientData> {
+  final TextEditingController sexController = TextEditingController();
+  Sex? selectedSex;
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final List<DropdownMenuEntry<Sex>> sexEntries = <DropdownMenuEntry<Sex>>[];
+    final formKey = GlobalKey<FormState>();
+    for (final Sex sex in Sex.values) {
+      sexEntries.add(DropdownMenuEntry(value: sex, label: sex.option));
+    }
+
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
             },
-            icon: Icon(Icons.arrow_back_ios),
+            icon: const Icon(Icons.arrow_back_ios),
           ),
-          title: Text("General"),
+          title: const Text("Datos del paciente"),
         ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/data_input');
-          },
-          child: Text("Continuar"),
-        )));
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Ingrese los datos del paciente"),
+                  sexDropDownMenu(sexEntries, width),
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 50,
+                        width: width * 0.40,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Peso (kg)",
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: width * 0.1,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: width * 0.40,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Edad (años)",
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
+
+  //---------------------------------WIDGETS-----------------------------------
+  DropdownMenu<Sex> sexDropDownMenu(
+      List<DropdownMenuEntry<Sex>> sexEntries, double width) {
+    return DropdownMenu(
+      controller: sexController,
+      dropdownMenuEntries: sexEntries,
+      label: const Text("Sexo"),
+      onSelected: (Sex? sexChosen) {
+        setState(() {
+          selectedSex = sexChosen;
+        });
+      },
+      width: width * 0.9,
+    );
+  }
+}
+
+enum Sex {
+  male('Masculino'),
+  female('Femenino');
+
+  const Sex(this.option);
+  final String option;
 }
