839f8775ac9f92f07e9bb2a08fe1659dfdccdb8d
Juan Pablo Díaz Correa <juanipis@gmail.com>
2023-09-02 21:10:00 -0500

    first working version of input

--- flutter/lib/presentation/screens/patient_data/patient_data.dart
+++ flutter/lib/presentation/screens/patient_data/patient_data.dart
@@ -60,81 +60,112 @@ class _PatientDataState extends State<PatientData> {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios),
-          ),
-          title: const Text("Datos del paciente"),
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Form(
-              key: formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text("Ingrese los datos del paciente"),
-                  sexDropDownMenu(sexEntries, width),
-                  Row(
-                    children: [
-                      textInput(width, weightController, "Peso (kg)"),
-                      separator(width),
-                      textInput(width, ageController, "Edad (años)"),
-                    ],
-                  ),
-                  textInput(width, creatinineController, "Creatinina (mg/dL)",
-                      widthScale: 0.90),
-                  SwitchForm(
-                      title: "Alergia a penicilina",
-                      width: width,
-                      widthScale: 0.9,
-                      icon: Icons.medical_services,
-                      value: penicillinAllergy,
-                      onChanged: onPenicillinAllergyChanged),
-                  SwitchForm(
-                      title: "Hemodiálisis",
-                      width: width,
-                      widthScale: 0.9,
-                      icon: Icons.medical_services,
-                      value: hemodialysis,
-                      onChanged: onHemodialysisChanged),
-                  SwitchForm(
-                      title: "CAPD",
-                      width: width,
-                      widthScale: 0.9,
-                      icon: Icons.medical_services,
-                      value: capd,
-                      onChanged: onCapdChanged),
-                  SwitchForm(
-                      title: "CRRT",
-                      width: width,
-                      widthScale: 0.9,
-                      icon: Icons.medical_services,
-                      value: crrt,
-                      onChanged: onCrrtChanged),
-                ],
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
+      appBar: AppBar(
+        leading: IconButton(
           onPressed: () {
-            logger.d("Sexo: ${selectedSex!.option}"
-                "\nPeso: ${weightController.text}"
-                "\nEdad: ${ageController.text}"
-                "\nCreatinina: ${creatinineController.text}"
-                "\nAlergia a penicilina: $penicillinAllergy"
-                "\nHemodialisis: $hemodialysis"
-                "\nCAPD: $capd"
-                "\nCRRT: $crrt");
+            Navigator.pop(context);
           },
-        ));
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: const Text("Datos del paciente"),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("Ingrese los datos del paciente"),
+                sexDropDownMenu(sexEntries, width),
+                Row(
+                  children: [
+                    textInput(width, weightController, "Peso (kg)"),
+                    separator(width),
+                    textInput(width, ageController, "Edad (años)"),
+                  ],
+                ),
+                textInput(width, creatinineController, "Creatinina (mg/dL)",
+                    widthScale: 0.90),
+                SwitchForm(
+                    title: "Alergia a penicilina",
+                    width: width,
+                    widthScale: 0.9,
+                    icon: Icons.medical_services,
+                    value: penicillinAllergy,
+                    onChanged: onPenicillinAllergyChanged),
+                SwitchForm(
+                    title: "Hemodiálisis",
+                    width: width,
+                    widthScale: 0.9,
+                    icon: Icons.medical_services,
+                    value: hemodialysis,
+                    onChanged: onHemodialysisChanged),
+                SwitchForm(
+                    title: "CAPD",
+                    width: width,
+                    widthScale: 0.9,
+                    icon: Icons.medical_services,
+                    value: capd,
+                    onChanged: onCapdChanged),
+                SwitchForm(
+                    title: "CRRT",
+                    width: width,
+                    widthScale: 0.9,
+                    icon: Icons.medical_services,
+                    value: crrt,
+                    onChanged: onCrrtChanged),
+                SizedBox(
+                  width: width * 0.9,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Ubicación de infección"),
+                        Spacer(),
+                        ElevatedButton(
+                            onPressed: () {}, child: Text("Seleccionar"))
+                      ]),
+                ),
+                SizedBox(
+                  width: width * 0.9,
+                  child: const Row(children: [
+                    Icon(Icons.medication),
+                    Text("Depuración creatinina"),
+                    Spacer(),
+                    Text("151"),
+                    SizedBox(width: 10),
+                    Text("mg/dL"),
+                  ]),
+                ),
+                SizedBox(
+                  width: width * 0.9,
+                  height: width * 0.1,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        logger.d("Sexo: ${selectedSex!.option}"
+                            "\nPeso: ${weightController.text}"
+                            "\nEdad: ${ageController.text}"
+                            "\nCreatinina: ${creatinineController.text}"
+                            "\nAlergia a penicilina: $penicillinAllergy"
+                            "\nHemodialisis: $hemodialysis"
+                            "\nCAPD: $capd"
+                            "\nCRRT: $crrt");
+                      },
+                      child: const Text("Continuar")),
+                ),
+                SizedBox(
+                  width: width * 0.9,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   //---------------------------------WIDGETS-----------------------------------
