9eceaef87cd8a2b43efcf24b172065d16dd690ca
Juan Pablo Díaz Correa <juanipis@gmail.com>
2023-09-04 12:02:21 -0500

    Fixed switch

--- flutter/lib/presentation/screens/patient_data/form_components.dart
+++ flutter/lib/presentation/screens/patient_data/form_components.dart
@@ -31,3 +31,18 @@ class SwitchForm extends StatelessWidget {
     );
   }
 }
+
+Widget switchTest(String title, double width, double widthScale, IconData icon,
+    bool value, Function onChange) {
+  return SizedBox(
+    width: width * widthScale,
+    child: SwitchListTile(
+      title: Text(title),
+      value: value,
+      onChanged: (newValue) {
+        onChange(newValue);
+      },
+      secondary: Icon(icon),
+    ),
+  );
+}
