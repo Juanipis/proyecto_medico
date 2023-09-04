df0658515ca5a68a6820811f231f041fa7891cc5
susiur <110204947+susiur@users.noreply.github.com>
2023-09-04 19:04:30 +0000

    Results antibiotics

--- flutter/lib/presentation/screens/antibiotic/antibiotic.dart
+++ flutter/lib/presentation/screens/antibiotic/antibiotic.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_medico/presentation/screens/results/results.dart';
 
-class Antibiotic extends StatelessWidget {
+class Antibiotic extends StatefulWidget {
+  const Antibiotic({super.key});
+
+  @override
+  State<Antibiotic> createState() => _AntibioticState();
+}
+class TextStyleExample extends StatelessWidget {
+  const TextStyleExample({
+    super.key,
+    required this.name,
+    required this.style,
+  });
+  final String name;
+  final TextStyle style;
+
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(name, style: style),
+    );
+  }
+}
+
+class _AntibioticState extends State<Antibiotic> {
+  String _selectedOption = '≤';
+  TextEditingController _textFieldController = TextEditingController();
+  final List<String> _options = ['≤', '≥', '='];
+
+  @override
+  Widget build(BuildContext context) {
+  final textTheme = Theme.of(context)
+      .textTheme
+      .apply(displayColor: Theme.of(context).colorScheme.onSurface);
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -14,12 +45,233 @@ class Antibiotic extends StatelessWidget {
           ),
           title: Text("Antibioticos"),
         ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/results');
-          },
-          child: Text("Continuar"),
-        )));
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                child: TextStyleExample(
+                    name: 'Escherichia Coli', style: textTheme.displayMedium!)),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Color(0xFFE0E3E7), width: 2)),
+                child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(6, 6, 6, 6),
+                    child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                              alignment: AlignmentDirectional(1, 0),
+                              child: Text('Amikacin')),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          DropdownButton<String>(
+                              value: _selectedOption,
+                              items: _options.map((String option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                              onChanged: (String? selectedOption) {
+                                setState(() {
+                                  _selectedOption = selectedOption!;
+                                });
+                              }),
+                          Spacer(),
+                          Align(
+                              alignment: AlignmentDirectional(0, 1),
+                              child: Container(
+                                  width: MediaQuery.sizeOf(context).width * 0.2,
+                                  child: TextField(
+                                      controller: _textFieldController,
+                                      decoration: InputDecoration(
+                                        labelText: 'MIC',
+                                      ))))
+                        ]))),
+            Spacer(),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Color(0xFFE0E3E7), width: 2)),
+                child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(6, 6, 6, 6),
+                    child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                              alignment: AlignmentDirectional(1, 0),
+                              child: Text('Ampicilin')),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          DropdownButton<String>(
+                              value: _selectedOption,
+                              items: _options.map((String option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                              onChanged: (String? selectedOption) {
+                                setState(() {
+                                  _selectedOption = selectedOption!;
+                                });
+                              }),
+                          Spacer(),
+                          Align(
+                              alignment: AlignmentDirectional(0, 1),
+                              child: Container(
+                                  width: MediaQuery.sizeOf(context).width * 0.2,
+                                  child: TextField(
+                                      controller: _textFieldController,
+                                      decoration: InputDecoration(
+                                        labelText: 'MIC',
+                                      ))))
+                        ]))),
+            Spacer(),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Color(0xFFE0E3E7), width: 2)),
+                child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(6, 6, 6, 6),
+                    child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                              alignment: AlignmentDirectional(1, 0),
+                              child: Text('Aztreonam')),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          DropdownButton<String>(
+                              value: _selectedOption,
+                              items: _options.map((String option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                              onChanged: (String? selectedOption) {
+                                setState(() {
+                                  _selectedOption = selectedOption!;
+                                });
+                              }),
+                          Spacer(),
+                          Align(
+                              alignment: AlignmentDirectional(0, 1),
+                              child: Container(
+                                  width: MediaQuery.sizeOf(context).width * 0.2,
+                                  child: TextField(
+                                      controller: _textFieldController,
+                                      decoration: InputDecoration(
+                                        labelText: 'MIC',
+                                      ))))
+                        ]))),
+            Spacer(),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Color(0xFFE0E3E7), width: 2)),
+                child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(6, 6, 6, 6),
+                    child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                              alignment: AlignmentDirectional(1, 0),
+                              child: Text('Aztreonam')),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          DropdownButton<String>(
+                              value: _selectedOption,
+                              items: _options.map((String option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                              onChanged: (String? selectedOption) {
+                                setState(() {
+                                  _selectedOption = selectedOption!;
+                                });
+                              }),
+                          Spacer(),
+                          Align(
+                              alignment: AlignmentDirectional(0, 1),
+                              child: Container(
+                                  width: MediaQuery.sizeOf(context).width * 0.2,
+                                  child: TextField(
+                                      controller: _textFieldController,
+                                      decoration: InputDecoration(
+                                        labelText: 'MIC',
+                                      ))))
+                        ]))),
+            Spacer(),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Color(0xFFE0E3E7), width: 2)),
+                child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(6, 6, 6, 6),
+                    child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Align(
+                              alignment: AlignmentDirectional(1, 0),
+                              child: Text('Ampicilin/Sulbactan')),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          DropdownButton<String>(
+                              value: _selectedOption,
+                              items: _options.map((String option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                              onChanged: (String? selectedOption) {
+                                setState(() {
+                                  _selectedOption = selectedOption!;
+                                });
+                              }),
+                          Spacer(),
+                          Align(
+                              alignment: AlignmentDirectional(0, 1),
+                              child: Container(
+                                  width: MediaQuery.sizeOf(context).width * 0.2,
+                                  child: TextField(
+                                      controller: _textFieldController,
+                                      decoration: InputDecoration(
+                                        labelText: 'MIC',
+                                      ))))
+                        ]))),
+            Spacer(),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/results');
+                },
+                child: Text("Continuar"),
+              ),
+            )
+          ]),
+        ));
   }
 }
--- flutter/lib/presentation/screens/results/results.dart
+++ flutter/lib/presentation/screens/results/results.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
+class TextStyleExample extends StatelessWidget {
+  const TextStyleExample({
+    super.key,
+    required this.name,
+    required this.style,
+  });
+  final String name;
+  final TextStyle style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(name, style: style),
+    );
+  }
+}
 class Results extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context)
+    .textTheme
+    .apply(displayColor: Theme.of(context).colorScheme.onSurface);
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -14,11 +34,119 @@ class Results extends StatelessWidget {
           title: Text("Resultados"),
         ),
         body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.popUntil(context, ModalRoute.withName('/'));
-          },
-          child: Text("Continuar"),
-        )));
+          child: Column(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Container(
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 4,
+                        color: Color(0x33000000),
+                        offset: Offset(0, 2),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(20, 5, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Resultados del análisis', style: textTheme.titleLarge!)
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(20, 5, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Bacteria: blbabalbalba', style: textTheme.labelLarge!),
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(20, 5, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Organo: Corazón', style: textTheme.labelLarge!),
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(50, 5, 0, 0),
+                              child: TextStyleExample(
+                                name: '- Germen blabla\n- Germen blabla 2', 
+                                style: textTheme.labelLarge!)
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(10, 5, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Resultados del tratamiento:', style: textTheme.labelLarge!)
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(10, 2, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Opción 1: Lorem ipsum dolor\n2 gml sav chora', 
+                                style: textTheme.labelLarge!)
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(10, 2, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Opción 2: Lorem ipsum dolor\n2 gml sav chora', 
+                                style: textTheme.labelLarge!)
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(10, 2, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Opción 2: Lorem ipsum dolor\n2 gml sav chora', 
+                                style: textTheme.labelLarge!)
+                            ),
+                          ),
+                          Align(
+                            alignment: AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(10, 2, 0, 0),
+                              child: TextStyleExample(
+                                name: 'Opción 2: Lorem ipsum dolor\n2 gml sav chora', 
+                                style: textTheme.labelLarge!)
+                            ),
+                          ),
+                        ],
+                      ),
+                ),
+                ),
+              SizedBox(
+                height: 50,
+              ),
+
+              ElevatedButton(
+                        onPressed: () {
+                          Navigator.popUntil(context, ModalRoute.withName('/'));
+                        },
+                        child: Text("Continuar"),
+                ),
+            ],
+          )));
   }
 }
