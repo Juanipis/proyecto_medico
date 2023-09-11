f2f0294fed0ebe24b9df6f8cd102a82814a3a92b
natynaro <90767304+natynaro@users.noreply.github.com>
2023-09-11 18:06:53 -0500

    Models

---
+++ java/backend/src/main/java/com/medico/backend/models/InfectionLocation.java
@@ -0,0 +1,28 @@
+package com.medico.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "infection_location")
+public class InfectionLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column
+    @Getter
+    @Setter
+    private String name;
+
+}
---
+++ java/backend/src/main/java/com/medico/backend/models/Patient.java
@@ -0,0 +1,63 @@
+package com.medico.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Patient")
+
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column
+    @Getter
+    @Setter
+    Character sex;
+
+    @Column
+    @Getter
+    @Setter
+    double weight;
+
+    @Column
+    @Getter
+    @Setter
+    int age;
+
+    @Column
+    @Getter
+    @Setter
+    Boolean creatinine;
+
+    @Column
+    @Getter
+    @Setter
+    Boolean hemodialisis;
+
+      @Column
+    @Getter
+    @Setter
+    Boolean capd;
+
+      @Column
+    @Getter
+    @Setter
+    Boolean crrt;
+
+    
+    @Getter
+    @Setter
+    Long infection_location_id;
+}
