4d2cfc1409898da9fba26e3687f9800eaff4bb06
Juan Pablo Díaz Correa <juanipis@gmail.com>
2023-09-24 19:42:44 -0500

    fix

--- flutter/lib/presentation/screens/patient_data/patient_data.dart
+++ flutter/lib/presentation/screens/patient_data/patient_data.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:proyecto_medico/presentation/screens/patient_data/form_components.dart';
 import 'package:logger/logger.dart';
 import 'package:proyecto_medico/presentation/screens/patient_data/body_dialog.dart';
+import 'package:proyecto_medico/presentation/text/tittle_material.dart';
 
 class PatientData extends StatelessWidget {
   const PatientData({Key? key}) : super(key: key);
@@ -79,140 +80,142 @@ class _FormTestState extends State<PatientDataState> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    final formKey = GlobalKey<FormState>();
     final List<DropdownMenuEntry<Sex>> sexEntries = <DropdownMenuEntry<Sex>>[];
     for (final Sex sex in Sex.values) {
       sexEntries.add(DropdownMenuEntry(value: sex, label: sex.option));
     }
+    final textTheme = Theme.of(context)
+        .textTheme
+        .apply(displayColor: Theme.of(context).colorScheme.onSurface);
 
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back),
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
+          title: const Text("Datos del paciente"),
         ),
-        title: const Text("Datos del paciente"),
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: width,
+            height: height * 0.95,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                MaterialText(
+                    name: "Ingrese los datos del paciente",
+                    style: textTheme.headlineSmall!),
+                sexDropDownMenu(sexEntries, width),
+                patientTextBoxes(width),
+                switches(width),
+                infectionLocationButton(width, context, height),
+                SizedBox(
+                  width: width * 0.9,
+                  child: const Row(children: [
+                    Icon(Icons.medication),
+                    Text("Depuración creatinina"),
+                    Spacer(),
+                    Text("151"),
+                    SizedBox(width: 10),
+                    Text("mg/dL"),
+                  ]),
+                ),
+                continueButton(width, context),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  SizedBox patientTextBoxes(double width) {
+    return SizedBox(
+      width: width * 0.9,
+      child: Column(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text("Ingrese los datos del paciente"),
-              sexDropDownMenu(sexEntries, width),
-              Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          textInput(width, weightController, "Peso (kg)"),
-                          separator(width),
-                          textInput(width, ageController, "Edad (años)")
-                        ],
-                      ),
-                      textInput(
-                          width, creatinineController, "Creatinina (mg/dL)",
-                          widthScale: 0.90),
-                    ],
-                  )),
-              Column(
-                children: [
-                  switchTest(
-                      switchsLabels[0],
-                      width,
-                      0.9,
-                      Icons.medical_services,
-                      penicillinAllergy,
-                      onPenicillinAllergyChanged),
-                  switchTest(
-                      switchsLabels[1],
-                      width,
-                      0.9,
-                      Icons.medical_services,
-                      hemodialysis,
-                      onHemodialysisChanged),
-                  switchTest(switchsLabels[2], width, 0.9,
-                      Icons.medical_services, capd, onCapdChanged),
-                  switchTest(switchsLabels[3], width, 0.9,
-                      Icons.medical_services, crrt, onCrrtChanged),
-                  SizedBox(
-                    width: width * 0.9,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text("Ubicación de infección: "),
-                              Text(selectedInfection < 0 &&
-                                      selectedInfection <= infections.length
-                                  ? "No seleccionada"
-                                  : infections[selectedInfection]),
-                            ],
-                          ),
-                          const Spacer(),
-                          ElevatedButton(
-                              onPressed: () async {
-                                int? selectedInfection = await bodyDialog(
-                                    context, height, width, infections);
-                                setState(() {
-                                  this.selectedInfection = selectedInfection!;
-                                });
-                              },
-                              child: const Text("Seleccionar"))
-                        ]),
-                  ),
-                  SizedBox(
-                    width: width * 0.9,
-                    child: const Row(children: [
-                      Icon(Icons.medication),
-                      Text("Depuración creatinina"),
-                      Spacer(),
-                      Text("151"),
-                      SizedBox(width: 10),
-                      Text("mg/dL"),
-                    ]),
-                  ),
-                  SizedBox(
-                    width: width * 0.9,
-                    height: 50,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          if (selectedSex == null) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text("Seleccione el sexo")));
-                            return;
-                          }
-                          logger.d("Sexo: ${selectedSex!.option}"
-                              "\nPeso: ${weightController.text}"
-                              "\nEdad: ${ageController.text}"
-                              "\nCreatinina: ${creatinineController.text}"
-                              "\nAlergia a penicilina: $penicillinAllergy"
-                              "\nHemodialisis: $hemodialysis"
-                              "\nCAPD: $capd"
-                              "\nCRRT: $crrt"
-                              "\nInfección: $selectedInfection");
-                          Navigator.pushNamed(context, '/data_input');
-                        },
-                        child: const Text("Continuar")),
-                  ),
-                  SizedBox(
-                    width: width * 0.9,
-                  )
-                ],
-              ),
+              textInput(width, weightController, "Peso (kg)"),
+              textInput(width, ageController, "Edad (años)")
             ],
           ),
+          textInput(width, creatinineController, "Creatinina (mg/dL)",
+              widthScale: 0.90),
         ],
       ),
+    );
+  }
+
+  SizedBox continueButton(double width, BuildContext context) {
+    return SizedBox(
+      width: width * 0.9,
+      height: 50,
+      child: ElevatedButton(
+          onPressed: () {
+            if (selectedSex == null) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Seleccione el sexo")));
+              return;
+            }
+            logger.d("Sexo: ${selectedSex!.option}"
+                "\nPeso: ${weightController.text}"
+                "\nEdad: ${ageController.text}"
+                "\nCreatinina: ${creatinineController.text}"
+                "\nAlergia a penicilina: $penicillinAllergy"
+                "\nHemodialisis: $hemodialysis"
+                "\nCAPD: $capd"
+                "\nCRRT: $crrt"
+                "\nInfección: $selectedInfection");
+            Navigator.pushNamed(context, '/data_input');
+          },
+          child: const Text("Continuar")),
+    );
+  }
+
+  SizedBox infectionLocationButton(
+      double width, BuildContext context, double height) {
+    return SizedBox(
+      width: width * 0.9,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Ubicación de infección: "),
+            Text(selectedInfection < 0 && selectedInfection <= infections.length
+                ? "No seleccionada"
+                : infections[selectedInfection]),
+          ],
+        ),
+        const Spacer(),
+        ElevatedButton(
+            onPressed: () async {
+              int? selectedInfection =
+                  await bodyDialog(context, height, width, infections);
+              setState(() {
+                this.selectedInfection = selectedInfection!;
+              });
+            },
+            child: const Text("Seleccionar"))
+      ]),
+    );
+  }
+
+  Column switches(double width) {
+    return Column(
+      children: [
+        switchTest(switchsLabels[0], width, 0.9, Icons.medical_services,
+            penicillinAllergy, onPenicillinAllergyChanged),
+        switchTest(switchsLabels[1], width, 0.9, Icons.medical_services,
+            hemodialysis, onHemodialysisChanged),
+        switchTest(switchsLabels[2], width, 0.9, Icons.medical_services, capd,
+            onCapdChanged),
+        switchTest(switchsLabels[3], width, 0.9, Icons.medical_services, crrt,
+            onCrrtChanged),
+      ],
     );
   }
 
